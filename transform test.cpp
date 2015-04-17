43e48cca199a47337be5474caae93d14094a313a
taobingxue <taobingxue001@126.com>
2015-04-17 15:55:10 +0800

    Great test! vote with affine, count with perspect
    parameter that limit the range to vote is iS and iE.
    iS = length / 3.8 is good for car
    iS = length * 0.4 is good for people
    maybe get twice sets or better range.

--- transform test.cpp
+++ transform test.cpp
@@ -10,6 +10,8 @@
 #include <cstdio>
 #include <iostream>
 #include <algorithm>
+#include <windows.h>
+#include "time.h"
 
 using namespace std;
 
@@ -25,7 +27,7 @@ void test(cv::Mat aa) {
 	cvDestroyWindow("show_image");
 }
 
-const int LIMITK = 500;				// limit for k
+const int LIMITK = 300;				// limit for k
 const int LIMITSUM = 1000000;		// limit for times we calculate affine transform matrix
 
 int main(int argc, char* argv[]) {
@@ -65,7 +67,10 @@ int main(int argc, char* argv[]) {
 	features1.resize(k);
 	features2.resize(k);
 	cout << "k = " << k <<endl;
-	/*
+
+	SYSTEMTIME t;
+	GetLocalTime(&t);
+	int startTime = t.wMilliseconds;
 	// calculate result directly with cv::findHomography
 	std::vector<uchar> inliers(features1.size());
 	cv::Mat result = cv::findHomography(	cv::Mat(features1),		// corresponding
@@ -78,8 +83,10 @@ int main(int argc, char* argv[]) {
 	cv::Mat mAfterTransform;
 	cv::warpPerspective(aaaaaa, mAfterTransform, result, aaaaaa.size());
 	cv::imwrite("ou1.jpg", mAfterTransform); 
-	*/
-
+	GetLocalTime(&t);
+	int nowTime = t.wMilliseconds;
+	cout << "time1:" << nowTime - startTime << endl;
+	startTime = nowTime;
 	/*
 	// calculate result directly with cv::estimateRigidTransform
 	cv::Mat result = cv::estimateRigidTransform(cv::Mat(features1),		// corresponding
@@ -92,6 +99,7 @@ int main(int argc, char* argv[]) {
 	cv::imwrite("ou2.jpg", mAfterTransform); 
 	*/
 
+	/*
 	// calculate result with vote based on cv::getAffineTransform()
 	// limit the scale of k
 	cv::Point2f pInput[3], pOuput[3];
@@ -111,15 +119,18 @@ int main(int argc, char* argv[]) {
 	
 	// iSumK = 5;
 	int sss = 0;
-	for (int i=0; i < iSumK; i++) {
+	int segLength = LIMITK / 3;
+	//for (int i=0; i < iSumK; i++) {
+	for (int i = 0; i < segLength; i ++) {
 		pInput[0] = features1[i];
 		pOuput[0] = features2[i];
-		for (int j=i+1; j < iSumK; j++) {
+		for (int j=segLength; j < segLength << 1; j++) {
 			pInput[1] = features1[j];
 			pOuput[1] = features2[j];
-			int step = (iSumK - j) < iTimes? 1 : (iSumK - j) / iTimes;   // step should > 0s
-			int p = j+1;
-			for (int q=0; q<iTimes && p<iSumK; q++) {
+			// int step = (iSumK - j) < iTimes? 1 : (iSumK - j) / iTimes;   // step should > 0s
+			// int p = j+1;
+			// for (int q=0; q<iTimes && p<iSumK; q++) {
+			for (int p = segLength << 1; p < LIMITK; p ++) {
 				pInput[2] = features1[p];
 				pOuput[2] = features2[p];
 				// get transform for i,j,q
@@ -134,23 +145,22 @@ int main(int argc, char* argv[]) {
 						dSortedTransforms[ii][jj].push_back(pdata[jj]);
 					}
 				}
-				p += step;
+				// p += step;
 
 				sss += 1;
-				if (sss % 100 == 0) cout << sss << endl;
+				if (sss % 100 == 0)	cout << sss << endl;
 			}
 		}
 	}
-
 	cout << endl << sss << endl;
 	cout << (int) dTransforms[0][0].size() << endl;
 	// sort values
 	for (int i = 0; i < 2; i ++)
-		for (int j = 0; j < 2; j ++)
+		for (int j = 0; j < 3; j ++)
 			sort(dSortedTransforms[i][j].begin(), dSortedTransforms[i][j].end());
 	// build result
 	double resultData[2][3];
-	int iMid = LIMITSUM >> 1;
+	int iMid = ((int) dTransforms[0][0].size()) >> 1;
 	for (int i = 0; i < 2; i ++) {
 		//pdata = result.ptr<double>(i);
 		for (int j = 0; j<3; j ++) {
@@ -165,5 +175,92 @@ int main(int argc, char* argv[]) {
 	// use the transform matrix
 	cv::Mat mAfterTransform;
 	cv::warpAffine(aaaaaa, mAfterTransform, result, aaaaaa.size());
-	cv::imwrite("ou2.jpg", mAfterTransform);  
+	cv::imwrite("ou2.jpg", mAfterTransform);
+	*/
+	
+	cv::Point2f pInput[3], pOuput[3];
+	vector<double> dTransforms[2][3], dSortedTransforms[2][3];
+	int sss = 0;
+	int isegLength = k / 3;
+	double* pdata;
+
+	for (int i = 0; i < isegLength; i ++) {
+		pInput[0] = features1[i];
+		pOuput[0] = features2[i];
+		pInput[1] = features1[i + isegLength];
+		pOuput[1] = features2[i + isegLength];
+		pInput[2] = features1[k - i - 1];
+		pOuput[2] = features2[k - i - 1];
+
+		cv::Mat mTransform = cv::getAffineTransform(pInput, pOuput);
+		for (int ii = 0; ii < 2; ii ++) {
+			pdata = mTransform.ptr<double>(ii);
+			for (int jj = 0; jj<3; jj ++) {
+				dTransforms[ii][jj].push_back(pdata[jj]);
+				dSortedTransforms[ii][jj].push_back(pdata[jj]);
+			}
+		}
+	}
+	cout << (int) dTransforms[0][0].size() << endl;
+	// sort values
+	for (int i = 0; i < 2; i ++)
+		for (int j = 0; j < 3; j ++)
+			sort(dSortedTransforms[i][j].begin(), dSortedTransforms[i][j].end());
+	// find features
+	vector<cv::Point2f> selectedFeaturesI, selectedFeaturesO;
+	int iS = isegLength / 3.8, iE = isegLength - iS;
+	cout << iS << " " << iE << endl;
+
+	/*freopen("a.out", "w", stdout);
+
+					for (int ia = 0; ia < 2; ia ++) {
+						for (int ja = 0; ja < 3; ja ++) cout << dSortedTransforms[ia][ja][iS] << "," << dSortedTransforms[ia][ja][iE] << " ";
+						cout << endl;
+					}*/
+	for (int i = 0; i < isegLength; i ++) {
+		bool flag = true;
+		for (int ii = 0; ii < 2; ii ++)
+			if (flag) for (int jj = 0; jj < 3; jj ++)
+				if (!(dTransforms[ii][jj][i] > dSortedTransforms[ii][jj][iS] && dTransforms[ii][jj][i] < dSortedTransforms[ii][jj][iE])) {
+					flag = false;
+					/*cout << i << " " << ii << " " << jj << endl;
+					for (int ia = 0; ia < 2; ia ++) {
+						for (int ja = 0; ja < 3; ja ++) cout << dTransforms[ia][ja][i] << " ";
+						cout << endl;
+					}*/
+					break;
+				}
+		if (flag) {
+			selectedFeaturesI.push_back(features1[i]);
+			selectedFeaturesO.push_back(features2[i]);
+			selectedFeaturesI.push_back(features1[i + isegLength]);
+			selectedFeaturesO.push_back(features2[i + isegLength]);
+			selectedFeaturesI.push_back(features1[k - i - 1]);
+			selectedFeaturesO.push_back(features2[k - i - 1]);
+		}
+	}
+	cout << selectedFeaturesI.size() << endl;
+	
+	// count result
+	std::vector<uchar> inlierss(selectedFeaturesI.size());
+	result = cv::findHomography(	cv::Mat(selectedFeaturesI),		// corresponding
+											cv::Mat(selectedFeaturesO),		// points
+											inlierss,				// outputted inliers matches
+											CV_RANSAC,				// RANSAC method
+											0.1);					// max distance to reprojection point
+	cout << result <<endl;
+	// use the transform matrix
+	//cv::Mat mAfterTransform;
+	cv::warpPerspective(aaaaaa, mAfterTransform, result, aaaaaa.size());
+	cv::imwrite("ou2.jpg", mAfterTransform); 
+	
+	GetLocalTime(&t);
+	nowTime = t.wMilliseconds;
+	cout << "time1:" << nowTime - startTime << endl;
+
+	cv::Mat cccccc = aaaaaa.clone();
+	cv::Scalar color( 255, 0, 0);
+	for (int i = 0; i < (int)selectedFeaturesI.size(); i ++)
+		cv::circle(cccccc, selectedFeaturesI[i], 3, color);
+	cv::imwrite("Pointed.jpg", cccccc);  
 }
